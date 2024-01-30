a4b26218fb0f910ce8e9cb6b2bedbb7f078c8953
syedanailagul <156816562+syedanailagul@users.noreply.github.com>
2024-01-30 15:54:46 +0500

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ARL
+#
+# Created:     30/01/2024
+# Copyright:   (c) ARL 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+pi=float(3.142)
+radius=int(input("enter radius of the circle"))
+areaofcircle=2*3.142*(radius*radius)
+print ("area of circle",areaofcircle)
---
+++ average of two numbers.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      ARL
+#
+# Created:     30/01/2024
+# Copyright:   (c) ARL 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+average=(num1+num2)/2
+print ("average:",average)
